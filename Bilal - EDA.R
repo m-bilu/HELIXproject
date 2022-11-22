761e170a5ed77e934f0ec7937653ffb5f4b5f3e0
m-bilu <02.mbilalkhan@gmail.com>
2022-11-22 14:11:37 -0500

    init EDA, data cleaning STILL IN PROGRESS

---
+++ Bilal - EDA.R
@@ -0,0 +1,31 @@
+load("C:/Users/mbila/Downloads/exposome_NA.RData")
+
+# CURRENT QUESTION: How well do mother's lifestyle covariates predict child's
+# Raven Test score. Also, which (out of postnatal and prenatal) affecct MORE?
+
+##############################################
+
+# Possible outcome variables in phenotypeNA:
+summary(phenotypeNA)
+# ID: representing unique mother-child pair
+# e3_bw: CHILDWEIGHT AT BIRTH
+codebook['hs_asthma', ]
+# e3_bw: Body mass index z-score at 6-11 years old
+codebook['hs_zbmi_who', ]
+# e3_bw: Total of correct answers at the RAVEN test
+codebook['hs_correct_raven', ]
+# e3_bw: Neuro Behaviour
+codebook['hs_Gen_Tot', ]
+# e3_bw: BMI at 6-11 yrs old
+codebook['hs_bmi_c_cat']
+
+##############################################
+#### GOALS OF EDA
+# Summarize our dataset
+# Visualize dataset
+# Identify missing values and come up with a cleaning strategy
+
+
+## 1) Summary
+## QUESTION: How much data do we include in our summary? Lifestyle Covariates
+
---
+++ Bilal - Intro, ideas.R
@@ -0,0 +1,58 @@
+load("C:/Users/mbila/Downloads/exposome_NA.RData")
+
+
+
+##################################################
+###################################################
+
+# THIS IS ALL OLD STUFF, OUTDATED
+# ONLY ON REPO FOR RECORDING PURPOSES
+
+##################################################
+##################################################
+
+
+
+## Data Summary:
+# codebook explains all variables in data
+# 3 datasets:
+# phenotypeNA holds health outcomes on measured on children at birth/6-11
+# exposomeNA holds environmental exposures of interest (main covariates),
+#   measured on the mother during preg/postnatally
+# covariatesNA holds covariates for either (extra)
+
+# ID represents unique key for mother-child pair (out of order in exposomeNA)
+
+#### GOALS:
+# - Answer specific question about dataset
+# - IMO use preset questions for efficiency
+# - MY OUTCOMES OF INTEREST (y's in models) must come from phenotypeNA dataset
+
+### Goals for exploratory data analysis:
+# - summary statistics, histograms, scatterplots, comment on interesting 
+# - findings, how they inform further analysis
+
+# CURRENT QUESTION: building the best possible predictive model for birthweight
+# what does exploring mean here?
+
+##############################################
+
+# Possible outcome variables in phenotypeNA:
+summary(phenotypeNA)
+# ID: representing unique mother-child pair
+# e3_bw: CHILDWEIGHT AT BIRTH
+codebook['hs_asthma', ]
+# e3_bw: Body mass index z-score at 6-11 years old
+codebook['hs_zbmi_who', ]
+# e3_bw: Total of correct answers at the RAVEN test
+codebook['hs_correct_raven', ]
+# e3_bw: Neuro Behaviour
+codebook['hs_Gen_Tot', ]
+# e3_bw: BMI at 6-11 yrs old
+codebook['hs_bmi_c_cat']
+
+##############################################
+#### GOALS OF EDA
+# Summarize our dataset
+# Visualize dataset
+# Identify missing values and come up with a cleaning strategy
