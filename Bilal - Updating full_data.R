c785abb20b3539caa180dac09b97265dab6834c2
m-bilu <02.mbilalkhan@gmail.com>
2022-11-24 12:33:32 -0500

    final corrections for full_data.csv

--- Bilal - Updating full_data.R
+++ Bilal - Updating full_data.R
@@ -1,21 +1,13 @@
-# FIXING full_data to include covariatesNA data as well
+##################
+## Creating full_data as union of covariatesNA, exposomeNA, hs_correct_raven
+##    First, we must sort data for ID
 load("C:/Users/mbila/Downloads/exposome_NA.RData")
 
-full_data <- read.csv('C:/Users/mbila/Documents/STAT 331 Final Project/full_data.csv')
+covariatesNASorted <- covariatesNA[order(covariatesNA$ID), ]
+correctRavenSorted <- phenotypeNA[order(phenotypeNA$ID), c('ID', 'hs_correct_raven')]
+exposomeNASorted <- exposomeNA[order(exposomeNA$ID), ]
 
-full_data <- cbind(full_data, covariatesNA[, 2:ncol(covariatesNA)])
+full_data <- cbind(correctRavenSorted, covariatesNASorted[, -1], exposomeNASorted[, -1])
 
-## Checking full_data is correct
-intersect(colnames(full_data), colnames(covariatesNA)) == colnames(covariatesNA)
-# intersect(full_data, covariatesNA) == covariatesNA
-
-##  Checking how many are lifestyle 
-##    (this may mean we need to update other datasets as well)
-lifestyleSubset <- codebook[codebook$family == 'Lifestyle', ]
-intersect(lifestyleSubset, covariatesNA)
-
-
-##  Double check concludes that no lifestyle covariates are in covariateNA,
-##    => Only need to update full_data
-##    Now we export
-write.csv(full_data, 'C:/Users/mbila/Documents/STAT 331 Final Project/full_data.csv')
+## Writing data out into csv:
+write.csv(full_data, 'C:/Users/mbila/Documents/STAT 331 Final Project/full_data_v2.csv')
