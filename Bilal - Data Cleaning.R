64810d0879c6c494ce88ba6ebc9a2745576a4fad
m-bilu <02.mbilalkhan@gmail.com>
2022-11-24 13:01:49 -0500

    cleaned data with mode imputation, next: predictive imputation

--- Bilal - Data Cleaning.R
+++ Bilal - Data Cleaning.R
@@ -10,16 +10,43 @@ load("C:/Users/mbila/Downloads/exposome_NA.RData")
 
 # First we get subset of Exploratory variables
 
-data <- read.csv('C:/Users/mbila/Documents/STAT 331 Final Project/full_data.csv')
-
+data <- read.csv('C:/Users/mbila/Documents/STAT 331 Final Project/full_data_v2.csv')
 
 ### Methods of cleaning
 # https://towardsdatascience.com/how-to-handle-missing-data-8646b18db0d4
 # - Regress covariates with missing data on all complete covariates,
 #     predict missing data
 # - Train model on Training Data (all complete), predict missing values
-# - For now, we replace with mean value, document what this does to variance,
-#     why this is appropriate option.
+# - CURRENT STRATEGY: 
+#     Mode Imputation (so numeric, non-numeric columns have same strategy)
+#     mean imputation does not apply to non-numeric columns
+
+
+#### STEP 1: Acquiring column modes
+modes <- rep(0, ncol(data))
+
+for (col in 1:ncol(data)) {
+  curmode <- mode(data[, col])
+  modes[col] <- curmode
+}
+
+## Now, modes is complete
+
+#### STEP 2: Assigning all NA's to same node value
+colnum <- 0
+cleandata <- sapply(data, function(col) {
+  colnum <- colnum + 1
+  for (i in 1:length(col)) {
+    col[i] = modes[colnum]
+  }
+})
+
+## Now, all NA's within one column hold same mode value
+## Sanity check:
+which(is.na(cleandata)) # Should be 0
+which(is.na(data)) # Should be non-zero vector
+
+write.csv(full_data, 'C:/Users/mbila/Documents/STAT 331 Final Project/full_clean_data_v1.csv')
 
 
 
