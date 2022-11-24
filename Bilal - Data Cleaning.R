66931b42040f85df6b31908514954b60e0f53aee
m-bilu <02.mbilalkhan@gmail.com>
2022-11-23 20:22:17 -0500

    old update to full_data, do not use

--- Bilal - Data Cleaning.R
+++ Bilal - Data Cleaning.R
@@ -1,5 +1,6 @@
 ###########################################
 # This file is for data imputation and cleaning.
+load("C:/Users/mbila/Downloads/exposome_NA.RData")
 
 # What are we counting as dirty data?
 # N/A values
@@ -10,15 +11,26 @@
 # First we get subset of Exploratory variables
 
 data <- read.csv('C:/Users/mbila/Documents/STAT 331 Final Project/full_data.csv')
-  
-# Now we select covariates of interest for our study
-# All lifestyle-tagged covariates pertaining data on the mother, post/prenatally
+
 
 ### Methods of cleaning
 # https://towardsdatascience.com/how-to-handle-missing-data-8646b18db0d4
 # - Regress covariates with missing data on all complete covariates,
 #     predict missing data
-# - Train model on Training Data (all complete), train
+# - Train model on Training Data (all complete), predict missing values
+# - For now, we replace with mean value, document what this does to variance,
+#     why this is appropriate option.
+
+
+
+
+
+
+
+
+
+## NOTE: # graph with MSPE, phi, lambda
+
 
 
 
---
+++ Bilal - Updating full_data.R
@@ -0,0 +1,21 @@
+# FIXING full_data to include covariatesNA data as well
+load("C:/Users/mbila/Downloads/exposome_NA.RData")
+
+full_data <- read.csv('C:/Users/mbila/Documents/STAT 331 Final Project/full_data.csv')
+
+full_data <- cbind(full_data, covariatesNA[, 2:ncol(covariatesNA)])
+
+## Checking full_data is correct
+intersect(colnames(full_data), colnames(covariatesNA)) == colnames(covariatesNA)
+intersect(full_data, covariatesNA) == covariatesNA
+
+##  Checking how many are lifestyle 
+##    (this may mean we need to update other datasets as well)
+lifestyleSubset <- codebook[codebook$family == 'Lifestyle', ]
+intersect(lifestyleSubset, covariatesNA)
+
+
+##  Double check concludes that no lifestyle covariates are in covariateNA,
+##    => Only need to update full_data
+##    Now we export
+write.csv(full_data, 'C:/Users/mbila/Documents/STAT 331 Final Project/full_data.csv')
